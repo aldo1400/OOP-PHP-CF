cf5113c0c1aef9178232827de052d8c2b3eb0e61
Aldo Choque <achoque1400@gmail.com>
2018-01-27 19:12:23 -0500

    Constructores y destructores

---
+++ Cap3/index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *
+ */
+class Loteria
+{
+  public $numero;
+  public $intentos;
+  public $resultado=false;
+
+  public function __construct($numero,$intentos)
+  {
+    $this->numero=$numero;
+    $this->intentos=$intentos;
+  }
+  public function sortear(){
+    $minimo=$this->numero/2;
+    $maximo=$this->numero*2;
+
+    for ($i=0; $i < $this->intentos; $i++) {
+      $int=rand($minimo,$maximo);
+      $this->intentos($int);
+    }
+  }
+  public function intentos($int){
+    if($int==$this->numero)
+    {
+
+      echo "<br>".$int."==".$this->numero."<br><br>";
+      $this->resultado=true;
+    }
+    else {
+      echo $int."!=".$this->numero."<br><br>";
+    }
+
+  }
+  public function __destruct(){
+    if($this->resultado)
+    {
+      echo "Felicidades has acertado en".$this->intentos."intentos";
+    }
+    else {
+      echo "Que lastima,has perdido en".$this->intentos."intentos";
+    }
+  }
+}
+
+$loteria=new Loteria(10,7);
+$loteria->sortear();
+ ?>
