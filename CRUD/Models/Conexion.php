2d82d566dd23c95bf9d24f4164bcbc803bef0f76
Aldo Choque <achoque1400@gmail.com>
2018-01-30 15:42:27 -0500

    Metodos de clase estudiante

--- CRUD/Models/Conexion.php
+++ CRUD/Models/Conexion.php
@@ -15,9 +15,18 @@ class Conexion
  private $con;
 
  public function __construct(){
-   $this->con=new mysqli($this->datos['host'],
-   $this->datos['user'],$this->['pass'],$this->datos['db']);
+   $this->con=new \mysqli($this->datos['host'],
+   $this->datos['user'],$this->datos['pass'],$this->datos['db']);
  }
+
+  public function consultaSimple($sql){
+    $this->con=query($sql);
+  }
+
+  public function consultaRetorno($sql){
+    $datos=$this->con->query($sql);
+    return $datos;
+  }
 
 }
 
